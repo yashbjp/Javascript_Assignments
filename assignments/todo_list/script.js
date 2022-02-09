014c06355579ec8574df2d2a33e2ca5fae940ef0
Yash <ybajpai48@gmail.com>
2022-02-09 13:26:05 +0530

    submit-java-assignment8

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,20 +1,75 @@
-var input=document.querySelector('input');
-var button=document.querySelector('button');
-var todoDiv=document.querySelector('div.todos');
+var input_text = document.querySelector("input[name='todo_input']")
+var btn = document.querySelector("button");
+var list_div = document.querySelector(".todos")
+var todos = document.querySelector(".todos");
+var dropdown = document.querySelector("#dropdown");
 
-let count=0;
-
-button.addEventListener('click', function(){
-    console.log(input.value);
-    if(input.value) {
-        var newTodo=document.createElement('p');
-        newTodo.innerHTML=input.value;
-        newTodo.setAttribute('key',count++);
-        todoDiv.appendChild(newTodo);
-        newTodo.addEventListener('click',function(e){
-            todoDiv.removeChild(e.target);
-        })
-        input.value="";
+btn.addEventListener("click", addToList);
+input_text.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        addToList(e);
     }
-})
+});
+var countItems = 0
+function addToList(){
+    var text = input_text.value;
+    if (text === null || text === ""){
+        alert("Please enter an item!!")
+    }
+    else{
+        var node = document.createElement("p");
+        var textNode = document.createTextNode(text);
+        node.setAttribute("key", countItems);
+        node.appendChild(textNode);
+        list_div.appendChild(node);
+        input_text.value = ""; input_text.focus();
+        countItems += 1;
+    }
+}
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.text())
+  .catch(function() {
+    console.error("Could not fetch data");
+  })
+  .then((data) => {
+    var todoArray = [];
 
+// To remove list item on click
+list_div.addEventListener("click", removeItem);
+function removeItem(e){
+    list_div.removeChild(e.target);
+}
+
+todoArray = data;
+addInList(todoArray)
+
+dropdown.addEventListener("change", populateList);
+
+function populateList(){
+  if(dropdown.value === "All"){
+    todoArray = data;
+    addInList(todoArray)
+  }
+  else if(dropdown.value === "Completed"){
+    todoArray = data.filter((item) => item.completed);
+    addInList(todoArray);
+  }
+  else{
+    todoArray = data.filter((item) => !item.completed);
+    addInList(todoArray);
+  }
+}
+})
+.catch(function() {
+console.error("Could not fetch data!");
+});
+function addInList(todoArray) {
+    todos.innerHTML = '';
+    todoArray.forEach((item) => {
+      var node = document.createElement("p");
+      node.setAttribute("key", item.id);
+      node.setAttribute("completed-status", item.completed)
+      node.innerText = item.title;
+      todos.appendChild(node);
+    });
+  } 
